cc6817061d3abcb70f44ddc3c73c77854cf7e394
germanp007 <pintogerman281@gmail.com>
2024-02-22 17:38:42 -0300

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,128 @@
+import { ChangeEvent, useContext, useState } from "react";
+import { ActionType, MyContext } from "./context/context";
 const App = () => {
+  const contextValue = useContext(MyContext);
+  const [bill, setBill] = useState<number | null>(null);
+  const [people, setPeople] = useState<number | null>(null);
+  const [custom, setCustom] = useState<number | null>(null);
+  console.log(bill, people, custom);
+  const handleBillChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBill(parseInt(e.target.value));
+  };
+  const handlePeopleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPeople(parseInt(e.target.value));
+  };
+
+  const handleCustomChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCustom(parseInt(e.target.value));
+  };
   return (
     <main className="bg-LightGrayishCyan w-screen h-[1140px] flex flex-col justify-center items-center gap-12">
       <h1 className="text-[23.84px] text-VeryDarkCyan tracking-[0.63rem] opacity-75">
         SPLI <br />
         TTER
       </h1>
-      <article className="bg-White w-full max-w-[500px]  h-[837px] rounded-2xl flex flex-col  items-center">
-        <div className="relative w-[80%] flex flex-col  items-center">
-          <h3 className="text-left">Bill</h3>
+      <article className="bg-White w-full max-w-[500px]  h-[837px] rounded-2xl flex flex-col  items-center justify-center gap-10">
+        <div className="relative w-[80%] flex flex-col">
+          <h3 className="text-left text-DarkGrayishCyan font-light">Bill</h3>
           <span className="absolute left-[1.1rem] inset-y-1/2 translate-y-1/2">
             <img
               src="../images/icon-dollar.svg"
               alt="dollar"
-              className="w-[12px] h-6"
+              className="w-[10px] h-5"
             />
           </span>
           <input
             type="number"
-            className="bg-VeryLightGrayishCyan w-full h-[47.66px] text-end outline-none text-VeryDarkCyan py-[0.365rem] px-[1.1rem]"
+            className="bg-VeryLightGrayishCyan w-full h-[47.66px] text-2xl text-end outline-StrongCyan text-VeryDarkCyan py-[0.365rem] px-[1.1rem]"
             placeholder="0"
           />
+        </div>
+        <div className="w-[80%]">
+          <h3 className="text-left text-DarkGrayishCyan font-light mb-4">
+            Select tip %
+          </h3>
+          <div className=" grid grid-cols-2 w-full m-auto gap-4 group-hover:bg-white">
+            <button
+              className="grid-span-1 transition-all duration-500"
+              value="5"
+            >
+              5%
+            </button>
+            <button
+              className="grid-span-1 transition-all duration-500"
+              value="10"
+            >
+              10%
+            </button>
+            <button
+              className="grid-span-1 transition-all duration-500"
+              value="15"
+            >
+              15%
+            </button>
+            <button
+              className="grid-span-1 transition-all duration-500"
+              value="25"
+            >
+              25%
+            </button>
+            <button
+              className="grid-span-1 transition-all duration-500"
+              value="50"
+            >
+              50%
+            </button>
+
+            <input
+              type="number"
+              className="bg-VeryLightGrayishCyan grid-span-1 w-full h-[47.66px] text-2xl text-end outline-StrongCyan text-VeryDarkCyan py-[0.365rem] px-[1.1rem]"
+              placeholder="Custom"
+            />
+          </div>
+        </div>
+        <div className="relative w-[80%] flex flex-col  items-center">
+          <h3 className="text-left text-DarkGrayishCyan font-light">
+            Number of people
+          </h3>
+          <span className="absolute left-[1.1rem] inset-y-1/2 translate-y-1/2">
+            <img
+              src="../images/icon-person.svg"
+              alt="dollar"
+              className="w-[14px] h-5"
+            />
+          </span>
+          <input
+            type="number"
+            className="bg-VeryLightGrayishCyan w-full h-[47.66px] text-2xl text-end outline-StrongCyan text-VeryDarkCyan py-[0.365rem] px-[1.1rem]"
+            placeholder="0"
+          />
+        </div>
+
+        <div className="bg-VeryDarkCyan rounded-lg w-[80%] h-[281px] text-white flex flex-col justify-center">
+          <div className="flex justify-between px-6 py-4">
+            <div>
+              <h3 className="font-normal">Tip Amount</h3>
+              <p className="font-normal text-GrayishCyan">/ person</p>
+            </div>
+            <div>
+              <h3 className="text-StrongCyan text-[32px]">$0.00</h3>
+            </div>
+          </div>
+          <div className="flex justify-between px-6 py-4">
+            <div>
+              <h3 className="font-normal">Total</h3>
+              <p className="font-normal text-GrayishCyan">/ person</p>
+            </div>
+            <div>
+              <h3 className="text-StrongCyan text-[32px]">$0.00</h3>
+            </div>
+          </div>
+          <div className="flex justify-between px-6 py-4">
+            <button className="w-full bg-StrongCyan text-VeryDarkCyan uppercase">
+              Reset
+            </button>
+          </div>
         </div>
       </article>
     </main>
--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,20 +1,92 @@
-import React, { ReactNode, createContext, useState } from "react";
+import React, { ReactNode, createContext, useReducer, useState } from "react";
 
-interface MyContextType {
+export interface MyContextType {
   value: number;
   setValue: React.Dispatch<React.SetStateAction<number>>;
+  state: MyInitialState;
+  dispatch: React.Dispatch<Action>;
 }
 
 interface MyContextProviderProps {
   children: ReactNode;
 }
 
-const MyContext = createContext<MyContextType | undefined>(undefined);
+export const MyContext = createContext<MyContextType | undefined>(undefined);
+
+interface MyInitialState {
+  bill: number | null;
+  people: number | null;
+  percent: number | string | null;
+  tipPerson: number;
+  totalPerson: number;
+}
+
+const initialState: MyInitialState = {
+  bill: null,
+  people: null,
+  percent: null,
+  tipPerson: 0,
+  totalPerson: 0,
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export enum ActionType {
+  UpdateBill = "UPDATE_BILL",
+  UpdatePeople = "UPDATE_PEOPLE",
+  UpdatePercent = "UPDATE_PERCENT",
+  UpdateTipPerson = "UPDATE_TIP_PERSON",
+  UpdateTotalPerson = "UPDATE_TOTAL_PERSON",
+  Reset = "RESET",
+}
+
+export interface Action {
+  type: ActionType;
+  payload: number | string | null;
+}
+
+const myReducer: React.Reducer<MyInitialState, Action> = (state, action) => {
+  switch (action.type) {
+    case ActionType.UpdateBill:
+      return {
+        ...state,
+        bill: action.payload,
+      };
+    case ActionType.UpdatePeople:
+      return {
+        ...state,
+        people: action.payload,
+      };
+    case ActionType.UpdatePercent:
+      return {
+        ...state,
+        percent: action.payload,
+      };
+    case ActionType.UpdateTipPerson:
+      return {
+        ...state,
+        tipPerson: (state.bill || 0 * (state.percent / 100)) / +state.people,
+      };
+    case ActionType.UpdateTotalPerson
+      return {
+        ...state,
+        totalPerson: state.bill + state.
+      }
+
+    default:
+      return state;
+  }
+};
 
 const MyContextProvider: React.FC<MyContextProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer<React.Reducer<MyInitialState, Action>>(
+    myReducer,
+    initialState
+  );
   const [value, setValue] = useState<number>(0);
 
   const contextValue: MyContextType = {
+    state,
+    dispatch,
     value,
     setValue,
   };
